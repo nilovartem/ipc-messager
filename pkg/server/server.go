4cf79a3382d212b52e0c8f39b4001b5fea154943
Artem <nilovtema@yandex.ru>
2023-06-21 11:37:10 +0300

    Add server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,134 +2,168 @@ package server
 
 import (
 	"bytes"
-	"encoding/gob"
-	"fmt"
-	"io"
-	"log"
+	"context"
 	"net"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/nilovartem/ipc-messager/pkg/auth"
 	"github.com/nilovartem/ipc-messager/pkg/client"
 )
 
+const (
+	DEFAULT_TIMEOUT = time.Duration(time.Millisecond * 100) // 100ms = default timeout for reading and writing
+)
+
+// A server is an entity that controls incoming messages and clients
 type server struct {
 	Certificate auth.Data
-	Clients     map[auth.PID]client.Client
-	gateway     string              //special socket for getting clients in
-	line        *chan client.Client //очередь клиентов, которые хотят подключиться к серверу. Их надо либо принять, либо откинуть
-	close       chan struct{}
+	clients     map[auth.PID]client.Client             //
+	gateway     string                                 //socket address
+	connection  *net.Conn                              //single connection
+	handler     func(request []byte) (response []byte) //custom function for handling messages
+	close       context.CancelFunc
+	errors      chan error    //common channel for errors
+	timeout     time.Duration //timeout for reading and writing
+	mutex       *sync.RWMutex
 }
 
-func (s *server) Accept(c client.Client) { //вернуть канал для чтения сообщений. и для отправки
-	//c.Accepted = true
-	fmt.Printf("Client %+v was accepted", c)
-	listener, _ := net.Listen("unix", c.Socket)
-	*c.Connection, _ = listener.Accept()
-
+// Block blocks reading messages from the client
+func (s *server) Block(c client.Client) {
+	c.Accepted = false
 }
 
-func New(gateway string) server {
-	fmt.Println("New server")
+// Allow allows reading messages from the client
+func (s *server) Allow(c client.Client) {
+	c.Accepted = true
+}
+
+// New creates new server and returns it
+func New(gateway string, timeout time.Duration, handler func(request []byte) (response []byte)) server {
 	s := server{
 		Certificate: auth.Current(),
-		Clients:     make(map[auth.PID]client.Client),
+		clients:     make(map[auth.PID]client.Client),
 		gateway:     gateway,
-		line:        nil, //небуферизованный канал
-		close:       make(chan struct{}, 1),
+		handler:     handler,
+		connection:  nil,
+		close:       nil,
+		timeout:     timeout,
+		mutex:       &sync.RWMutex{},
 	}
-	s.cleanup() //TODO: может и не надо чистить
 	return s
 }
 
-// Проверяет входящих клиентов на наличие сертификата и ставит в очередь
-func (s *server) serveGateway() {
-	listener, err := net.Listen("unix", s.gateway)
-	//fmt.Println(listener.Addr().String())
-	if err != nil {
-		//return err
+// serve reads connection and dispatches clients to handle()
+func (s *server) serve(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		{
+			s.cleanup()
+			return
+		}
+	default:
+		{
+			listener, err := net.Listen("unix", s.gateway)
+			if err != nil {
+				s.errors <- err
+				s.Close()
+
+			}
+			defer listener.Close()
+			for {
+				incoming, err := listener.Accept()
+				if err != nil {
+					s.errors <- err
+				}
+				go s.handle(incoming)
+			}
+
+		}
 	}
-	defer listener.Close()
+
+}
+
+// lookup checks if the connection with the client is maintained
+func (s *server) lookup(pid auth.PID) (ok bool) {
+	_, ok = s.clients[pid]
+	return
+}
+
+// Receive reads bytes from connection
+func (s *server) Receive(c net.Conn) (int, []byte) {
+	c.SetReadDeadline(time.Now().Add(s.timeout)) //100ms на чтение
+	var b []byte
+	var inc bytes.Buffer
+	inc.ReadFrom(c)
+	b = inc.Bytes()
+	return inc.Len(), b
+}
+
+// Send sends bytes to connection
+func (s *server) Send(c net.Conn, data []byte) {
+	c.SetWriteDeadline(time.Now().Add(s.timeout)) //100ms на запись
+	inc := bytes.NewBuffer(data)
+	inc.WriteTo(c)
+}
+
+// handle controls steps of converting the received data to messages and vice versa
+func (s *server) handle(c net.Conn) {
 	for {
-		//<-s.close
-		// Accept new connections, dispatching them to echoServer
-		// in a goroutine.
-		incoming, err := listener.Accept()
-		if err != nil {
-			log.Fatal("accept error:", err)
+		if length, b := s.Receive(c); length != 0 {
+			m := client.Message{}
+			if err := m.Unmarshall(b); err == nil {
+				if !s.lookup(m.Author.Certificate.PID) {
+					(*s.mutex).Lock()
+					s.clients[m.Author.Certificate.PID] = m.Author
+					(*s.mutex).Unlock()
+				}
+				if m.Author.Accepted {
+					response := s.handler(m.Content)
+					var buffer bytes.Buffer = *bytes.NewBuffer(response)
+					if buffer.Len() != 0 {
+						err := m.CreateMessage(response)
+						if err != nil {
+							s.errors <- err
+						}
+						response, err = m.Marshall()
+						if err != nil {
+							s.errors <- err
+						}
+						s.Send(c, response)
+					}
+				}
+			} else {
+				s.errors <- err
+			}
 		}
 
-		//первое сообщение, отправленное клиентом - сертификат
-		if valid, c, err := verifyClient(incoming); valid {
-			fmt.Println("valid")
-			//добавляем клиента в список активных
-			s.Clients[c.Certificate.PID] = c
-			fmt.Println("gateway client address ", &c)
-			*s.line <- c
-			fmt.Println("Line length ", len(*s.line))
-		} else {
-			log.Fatal("client error:", err)
-		}
-		incoming.Close()
-		/*client := Client{
-			certificate: ,
-		}*/
-
-		go echoServer(incoming)
 	}
+
 }
 
-// канал для прослушивания и ошибка и может быть канал для ошибок
-func (s *server) Listen() (<-chan client.Client, error) {
-	//fmt.Println("listen")
-	//line := make(chan client) //очередь клиентов, которые хотят подключиться к серверу. Их надо либо принять, либо откинуть
-	go s.serveGateway()
-	line := make(chan client.Client)
-	s.line = &line
-	return line, nil
+// Listen starts listening on server and returns channel for all errors that might occur
+func (s *server) Listen() <-chan error {
+	ctx := context.Background()
+	ctx, close := context.WithCancel(ctx)
+	s.close = close
+	go s.serve(ctx)
+	errors := make(chan error)
+	s.errors = errors
+	return errors
 }
+
+// Close initiate closing and cleanup processes
 func (s *server) Close() {
-	s.close <- struct{}{}
-	close(s.close)
+	s.close()
+	s.cleanup()
+
 }
+
+// cleanup closes server and all connections
 func (s *server) cleanup() {
-	if err := os.RemoveAll(s.gateway); err != nil {
-		log.Fatal(err)
+	if s.connection != nil {
+		_ = (*s.connection).Close()
 	}
-}
-
-// проверить клиента на валидность сертификата
-func verifyClient(incoming net.Conn) (bool, client.Client, error) {
-
-	//Шаблон для клиента
-	c := client.Client{
-		Certificate: auth.Data{},
-		Socket:      "",
-		Connection:  nil,
-		//messages:    make(chan interface{}),
-	}
-
-	var buffer bytes.Buffer
-	buffer.ReadFrom(incoming)
-	certificate := auth.Data{}
-	enc := gob.NewDecoder(&buffer)
-	err := enc.Decode(&certificate)
-
-	//fmt.Printf("Incoming := %+v", certificate)
-	//fmt.Println(buffer.Bytes())
-
-	if err != nil {
-		return false, c, err
-	}
-	c.Certificate = certificate
-	c.Connection = &incoming
-	//TODO: make good path and make it simple
-	c.Socket = "/tmp/" + c.Certificate.PID.String() + ".sock"
-	//fmt.Printf("All good : %+v", c)
-	return true, c, err
-}
-func echoServer(c net.Conn) {
-	log.Printf("Client connected [%s]", c.RemoteAddr().Network())
-	io.Copy(c, c)
-	c.Close()
+	os.RemoveAll(s.gateway)
 }
