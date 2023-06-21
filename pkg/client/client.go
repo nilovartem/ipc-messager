fe2d15a17346a9a88ecc002ee53b92d3875a984e
Artem <nilovtema@yandex.ru>
2023-06-21 11:37:54 +0300

    Add client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -5,87 +5,127 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
-	"log"
 	"net"
-	"os"
 	"time"
 
 	"github.com/nilovartem/ipc-messager/pkg/auth"
 )
 
+const (
+	DEFAULT_TIMEOUT = time.Duration(time.Millisecond * 100) // 100ms = default timeout for reading and writing
+)
+
+// A message contains content and author of received or sended message
+type Message struct {
+	Author  Client
+	Content []byte
+}
+
+// Method CreateMessage creates new Message and implementing IMessage
+func (m *Message) CreateMessage(data []byte) error {
+	c := Client{
+		Certificate: auth.Current(), //подставляем данные сервера
+		Accepted:    true,
+		connection:  nil,
+	}
+	m.Author = c
+	m.Content = data
+	return nil
+}
+
+// Method Marshall encodes Message to byte array
+func (m *Message) Marshall() ([]byte, error) {
+	var buffer bytes.Buffer
+	enc := gob.NewEncoder(&buffer)
+	err := enc.Encode(m)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+// Method Unmarshall decodes byte array to Message
+func (m *Message) Unmarshall(request []byte) error {
+	c := Client{
+		Certificate: auth.Data{},
+		Accepted:    true,
+	}
+	m.Author = c
+	var buffer bytes.Buffer = *bytes.NewBuffer(request)
+	enc := gob.NewDecoder(&buffer)
+	err := enc.Decode(m)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// A Client represents a connection to server
 type Client struct {
 	Certificate auth.Data //client's credentials
-	Socket      string
-	Connection  *net.Conn
-	//Accepted    bool //по умолчанию - не обслуживается - нужно выгадать время с таймаутом
+	Accepted    bool      //по умолчанию - не обслуживается - нужно выгадать время с таймаутом
+	connection  net.Conn
+	timeout     time.Duration
 }
 
-func (c *Client) accept() { //вернуть канал для чтения сообщений. и для отправки
-	//c.accepted = true
-	fmt.Printf("Client %+v was accepted", c)
-	fmt.Println("Before Dial = ", c.Connection)
-	fmt.Println(c.Socket)
-	con, err := net.Dial("unix", c.Socket)
-	if err == nil {
-		fmt.Println(c.Connection)
-		//TODO: add error management in net.dial
-		c.Connection = &con
-	} else {
-		fmt.Print(err.Error())
+// Method Send sends byte array to connection in Client
+func (c *Client) Send(data []byte) error {
+	c.connection.SetWriteDeadline(time.Now().Add(c.timeout)) //100ms на запись
+	m := Message{}
+	m.CreateMessage(data)
+	data, err := m.Marshall()
+	if err != nil {
+		return err
 	}
-
+	inc := bytes.NewBuffer(data)
+	inc.WriteTo(c.connection)
+	return nil
 }
 
-// Сообщение, отправляемое клиентом
-type message struct {
-	certificate auth.Data
-	body        []byte
-}
+// Method Receive receives byte array from connection in Client
+func (c *Client) Receive() ([]byte, bool) {
+	c.connection.SetReadDeadline(time.Now().Add(c.timeout)) //100ms на чтение
+	var b []byte
+	var inc bytes.Buffer
+	inc.ReadFrom(c.connection)
+	b = inc.Bytes()
+	if inc.Len() == 0 {
+		return nil, false
+	}
+	m := Message{}
+	err := m.Unmarshall(b)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return m.Content, true
 
+}
 func Connect(server string, timeout time.Duration) (Client, error) {
 	c := Client{
 		Certificate: auth.Current(),
-		Socket:      "",
-		Connection:  nil,
+		Accepted:    true,
+		timeout:     timeout,
 	}
-	//cleanup
-	c.auth()
-	//TODO: добавить таймер для таймаута
-	//теперь нужно проверить, создался ли сокет /tmp/clientPID.sock . Если создался, то все успешно, если нет - ошибка по timeout
 	timer := time.NewTimer(timeout)
 	for {
 		select {
 		case <-timer.C:
 			{
-				return c, errors.New("timer fired. cant establish final connection")
+				return c, errors.New("can't connect to server")
 			}
 		default:
 			{
-				if _, err := os.Stat("/tmp/" + c.Certificate.PID.String() + ".sock"); err == nil {
-					c.Socket = "/tmp/" + c.Certificate.PID.String() + ".sock"
-					c.accept()
+				conn, _ := net.Dial("unix", server)
+				if conn != nil {
+					c.connection = conn
 					return c, nil
 				}
 			}
 		}
 	}
 }
-func (c *Client) auth() {
-	conn, err := net.Dial("unix", "/tmp/server.sock")
-	if err != nil {
-		log.Fatal(err)
-	}
-	var buffer bytes.Buffer
-	enc := gob.NewEncoder(&buffer)
-	err = enc.Encode(c.Certificate)
-	fmt.Printf("Sending := %+v", c.Certificate)
-	fmt.Println(buffer.Bytes())
-	_, err = conn.Write(buffer.Bytes())
-	if err != nil {
-		log.Fatal("write error:", err)
-	}
-	conn.Close()
-}
 func (c *Client) Disconnect() {
-	//disconnect
+	if c.connection != nil {
+		c.connection.Close()
+	}
 }
